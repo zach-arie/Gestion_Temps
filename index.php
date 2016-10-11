67ec858593c7406eaa2430214e92d4da783d3da7
zach-arie <zacharie.fluz@gmail.com>
2016-10-11 10:21:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,16 +16,9 @@ and open the template in the editor.
 <?php 
     include("General/MenuGeneral.php");
 ?>
-    <div id="corps">
-        <h1>Mon super site</h1>
-        <p>
-            Bienvenue sur mon super site !<br />
-            Vous allez adorer ici, c'est un site génial qui va parler de... euh... Je cherche encore un peu le thème de mon site. :-D
-        </p>
-    </div>
     <!-- Le pied de page -->
     <footer id="pied_de_page">
-        <p>Copyright moi, tous droits réservés</p>
+        <p>Copyright zach, tous droits réservés</p>
     </footer>
     </body>
 </html>
