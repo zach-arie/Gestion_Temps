72ef7b2ca019b77045e1965698055a345072dc8a
zach-arie <zacharie.fluz@gmail.com>
2016-10-11 10:19:34 +0200

    Add files via upload

---
+++ General/MenuGeneral.php
@@ -0,0 +1,22 @@
+<!-- Menu général des pages -->
+<nav class="EMenu">
+	<h1 class="TitreMenu">Catalogue</h1>
+	<ul>
+		<li><a href="/Catalogue/Page_Oeuvre.php">Oeuvre</a></li>
+		<li><a href="#">Editions</a></li>
+		<li><a href="#">Bibliotheque</a></li>
+	</ul>
+	<h1 class="TitreMenu">General</h1>
+	<ul>
+		<li><a href="/Catalogue/General/compositeurs.php">Compositeurs</a></li>
+		<li><a href="#">Editeurs</a></li>
+		<li><a href="#">Bibliotheque</a></li>
+	</ul>
+	<h1 class="TitreMenu">Parametrages</h1>
+	<ul>
+		<li><a href="/Catalogue/Page_Parametrage.php">Parametrages</a></li>
+		<li><a href="#">Bibliotheque</a></li>
+		<li><a href="/Catalogue/General/BDD/Identification.php">Identification</a></li>
+	</ul>
+</nav>
+
